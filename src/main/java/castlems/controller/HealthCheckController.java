356757eb4600f2380ff0c1784bfbe5fe1224c7f6
Ivan Frolov <Ivan.Frolov@bsc-ideas.com>
2020-06-30 23:03:36 +0300

    Add healthchek

---
+++ src/main/java/castlems/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package castlems.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HealthCheckController {
+
+    @ResponseBody
+    @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
+    public String healthCheck() {
+        return "Healthcheck v 0.1";
+    }
+}
